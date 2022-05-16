ccf21e08b03233904a9ea8a83329a438cbd9a50b
terish-vividreal <terish.d@vividreal.com>
2022-05-16 15:51:26 +0530

    Company Bug fix

--- resources/views/companies/create.blade.php
+++ resources/views/companies/create.blade.php
@@ -64,8 +64,8 @@
                                 <label for="name" class="label-placeholder active"> Name <span class="red-text">*</span></label>
                             </div>
                             <div class="input-field col m6 s12">
-                                {!! Form::text('contactName', $company->contact_name ?? '', array('id' => 'contactName')) !!}
-                                <label for="contactName" class="label-placeholder active"> Contact Name <span class="red-text">*</span></label>
+                                {!! Form::text('contact_name', $company->contact_name ?? '', array('id' => 'contactName')) !!}
+                                <label for="contact_name" class="label-placeholder active"> Contact Name <span class="red-text">*</span></label>
                             </div>
                             <div class="input-field col m6 s12">
                                 {!! Form::text('email', $company->email ?? '', array('id' => 'email')) !!}
